ea56ec5a8ed3e1bfc89e9a6f7d3a31fe1ccc333c
Fred Blundun <fred@snowplowanalytics.com>
2014-08-05 12:52:37 +0100

    Added test for base 64 encoding

--- tests/tracker.js
+++ tests/tracker.js
@@ -60,7 +60,7 @@ describe('tracker', function () {
 	})
 
 	describe('#trackPageView', function () {
-		
+
 		it('should send a page view event', function (done) {
 			var expected = {
 				tv: 'node-' + version,
@@ -219,7 +219,8 @@ describe('tracker', function () {
 				res: '400x200',
 				vp: '500x800',
 				cd: '24',
-				tz: 'Europe London'
+				tz: 'Europe London',
+				dtm: '1000000000000'
 			};
 			var t = tracker('d3rkrsqld9gmqf.cloudfront.net', 'cf', 'cfe35', false, function(){
 				checkQuerystring(extractQueryString(), expected);
@@ -232,7 +233,26 @@ describe('tracker', function () {
 			t.setColorDepth(24);
 			t.setTimezone('Europe London');
 
-			t.trackPageView('http://www.example.com', 'example page', 'google', context);
+			t.trackPageView('http://www.example.com', 'example page', 'google', context, 1000000000000);
 		});
 	});	
+
+	describe('base 64 encoding', function () {
+
+		it('should base 64 encode unstructured events and custom contexts', function (done) {
+			var inputJson = {
+				schema: 'iglu:com.acme/viewed_product/jsonschema/1-0-0',
+				data: {
+					price: 20
+				}
+			};
+			var t = tracker('d3rkrsqld9gmqf.cloudfront.net', 'cf', 'cfe35', true, function(){
+				var qs = extractQueryString();
+				assert.equal(qs['ue_px'], 'eyJzY2hlbWEiOiJpZ2x1OmNvbS5zbm93cGxvd2FuYWx5dGljcy5zbm93cGxvdy91bnN0cnVjdF9ldmVudC9qc29uc2NoZW1hLzEtMC0wIiwiZGF0YSI6eyJzY2hlbWEiOiJpZ2x1OmNvbS5hY21lL3ZpZXdlZF9wcm9kdWN0L2pzb25zY2hlbWEvMS0wLTAiLCJkYXRhIjp7InByaWNlIjoyMH19fQ');
+				assert.equal(qs['cx'], 'eyJzY2hlbWEiOiJpZ2x1OmNvbS5zbm93cGxvd2FuYWx5dGljcy5zbm93cGxvdy9jb250ZXh0cy9qc29uc2NoZW1hLzEtMC0wIiwiZGF0YSI6W3sic2NoZW1hIjoiaWdsdTpjb20uYWNtZS91c2VyL2pzb25zY2hlbWEvMS0wLTAiLCJkYXRhIjp7InR5cGUiOiJ0ZXN0ZXIifX1dfQ');
+				done.apply(this, arguments);
+			});
+			t.trackUnstructEvent(inputJson, context);
+		});
+	});		
 });
