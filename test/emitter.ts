f533c7eae3030d242e65434f6e1bbbe4f109ab0b
Paul Boocock <paul@snowplowanalytics.com>
2020-09-01 15:33:21 +0100

    Add support for dvce_sent_tstamp (close #27)

--- test/emitter.ts
+++ test/emitter.ts
@@ -12,11 +12,10 @@
  */
 
 import test from 'ava';
-
+import sinon from 'sinon';
 import nock from 'nock';
 import { emitter } from '../src/index';
 import { HttpMethod, HttpProtocol } from '../src/emitter';
-
 
 const endpoint = 'd3rkrsqld9gmqf.cloudfront.net';
 
@@ -30,7 +29,7 @@ nock('http://' + endpoint, {
     return '/';
   })
   .get('/')
-	.reply(200, function (uri) {
+  .reply(200, function (uri) {
     return uri;
   });
 
@@ -45,9 +44,10 @@ nock('http://' + endpoint, {
     return '*';
   })
   .post('/com.snowplowanalytics.snowplow/tp2', '*')
-	.reply(200, function (_uri, body: Record<string, unknown>) {
+  .reply(200, function (_uri, body: Record<string, unknown>) {
     return (body['data'] as Array<unknown>)[0];
   });
+
 
 test.before(() => {
   nock.disableNetConnect();
@@ -58,39 +58,77 @@ test.after(() => {
 });
 
 test.cb('Emitter should send an HTTP GET request', (t) => {
-	const e = emitter(endpoint, HttpProtocol.HTTP, 80, HttpMethod.GET, undefined, function (error, _body, response) {
-		t.is(response, '/i?a=b');
-		t.end(error);
+  const e = emitter(endpoint, HttpProtocol.HTTP, 80, HttpMethod.GET, undefined, function (error, _body, response) {
+    t.regex(response, /\/i\?.*a=b.*/);
+    t.end(error);
   });
-	e.input({ a: 'b' });
+  e.input({ a: 'b' });
 });
 
 test.cb('Emitter should send an HTTP POST request', (t) => {
   const e = emitter(endpoint, HttpProtocol.HTTP, undefined, HttpMethod.POST, 1, function (error, _body, response) {
-		t.deepEqual(response, { a: 'b' });
-		t.end(error);
+    t.like(response, { a: 'b' });
+    t.end(error);
   });
   e.input({ a: 'b' });
 });
 
 test.cb('should send an HTTPS GET request', (t) => {
   const e = emitter(endpoint, HttpProtocol.HTTPS, 443, HttpMethod.GET, undefined, function (error, _body, response) {
-		t.is(response, '/i?a=b');
-		t.end(error);
+    t.regex(response, /\/i\?.*a=b.*/);
+    t.end(error);
   });
   e.input({ a: 'b' });
 });
 
-test.cb('should not send requests if the buffer is not full', (t) => {
-  const e = emitter(endpoint, HttpProtocol.HTTPS, undefined, HttpMethod.POST, undefined, () => t.fail('Event unexpectedly emitted'));
-  e.input({});
-  e.input({});
-	e.input({});
-	setTimeout(t.end, 250); //Give chance for emitter callback to fire
+test.cb('Emitter should send an HTTPS POST request', (t) => {
+  const e = emitter(endpoint, HttpProtocol.HTTPS, undefined, HttpMethod.POST, 1, function (error, _body, response) {
+    t.like(response, { a: 'b' });
+    t.end(error);
+  });
+  e.input({ a: 'b' });
 });
 
-test.cb('should not send requests if the buffer is empty', (t) => {
-	const e = emitter(endpoint, HttpProtocol.HTTPS, undefined, HttpMethod.POST, undefined, () => t.fail('Event unexpectedly emitted'));
-	e.flush();
-	setTimeout(t.end, 250); //Give chance for emitter callback to fire
+test.cb('Emitter should not send requests if the buffer is not full', (t) => {
+  const e = emitter(endpoint, HttpProtocol.HTTPS, undefined, HttpMethod.POST, undefined, () =>
+    t.fail('Event unexpectedly emitted')
+  );
+  e.input({});
+  e.input({});
+  e.input({});
+  setTimeout(t.end, 250); //Give chance for emitter callback to fire
+});
+
+test.cb('Emitter should not send requests if the buffer is empty', (t) => {
+  const e = emitter(endpoint, HttpProtocol.HTTPS, undefined, HttpMethod.POST, undefined, () =>
+    t.fail('Event unexpectedly emitted')
+  );
+  e.flush();
+  setTimeout(t.end, 250); //Give chance for emitter callback to fire
+});
+
+test.cb('Emitter should add STM querystring parameter when sending POST requests', (t) => {
+  const testTime = new Date('1988-12-12T12:30:00.000Z').getTime();
+  const clock = sinon.useFakeTimers(testTime);
+
+  const e = emitter(endpoint, HttpProtocol.HTTP, undefined, HttpMethod.POST, 1, function (error, _body, response) {
+    t.like(response, { stm: testTime.toString() });
+    t.end(error);
+  });
+  e.input({ a: 'b' });
+
+  clock.restore();
+});
+
+test.cb('Emitter should add STM querystring parameter when sending GET requests', (t) => {
+  const testTime = new Date('2020-06-15T09:12:30.000Z').getTime();
+  const clock = sinon.useFakeTimers(testTime);
+
+  const e = emitter(endpoint, HttpProtocol.HTTPS, 443, HttpMethod.GET, undefined, function (error, _body, response) {
+    t.regex(response, new RegExp(`/i?.*stm=${testTime}.*`));
+    t.end(error);
+  });
+  e.input({ a: 'b' });
+
+  clock.restore();
 });
